a91778cdc91253db65d82cd9a2147572da789c4e
Alex Camargo <alexcamargo13@gmail.com>
2020-06-29 15:12:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function theBeatlesPlay(music, instrument){
   var arr =[]
   for( var i =0;i<music.length;i++){
     last += music[i]+" plays "+instrument[i]
-    
+    arr.push(last)
   }
+  return arr
 }
